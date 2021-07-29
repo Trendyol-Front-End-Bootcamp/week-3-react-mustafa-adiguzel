7436cdfe752fc27e023350174b849cd435ece202
Mustafa <mustafa.adiguzel@std.izmirekonomi.edu.tr>
2021-07-29 14:08:51 +0300

    reflections completed, load more done, search fixed

--- src/components/Character/CharacterList.js
+++ src/components/Character/CharacterList.js
@@ -1,25 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Character from "./Character";
-import axios from "axios";
+
 
 export default function CharacterList(props) {
-    const [CharacterArr, GetCharacterArr] = useState([]);
-
-    useEffect(() => {
-      axios
-        .get("https://rickandmortyapi.com/api/character/")
-        .then(response => {
-          GetCharacterArr(response.data.results);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-
-    }, []);
-    
     return (
       <section className="character-list">
-        {CharacterArr.map((character, id) => {
+        {props.characters.map((character, id) => {
           return <div>
               <Character key={id} character={character} />
             </div>;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,27 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Search() {
+
+export default function Search(props) {
+
+  const [filterParameters, setfilterParameters] = useState({});
+  
+  const handleGenderChange = (event) => { 
+     const val = event.target.value;
+
+     if(val){
+       setfilterParameters({
+         ...filterParameters,
+         gender: val
+       })
+     }
+  }
+
+  const handleStatusChange = (event) => { 
+    const val = event.target.value;
+
+    if(val){
+      setfilterParameters({
+        ...filterParameters,
+        status: val
+      })
+    }
+  }
+
+  const handleSearch = (event) => {
+      event.preventDefault();
+      props.onSearch(filterParameters);
+  }
+
   return (
     <div className="top-search">
         <div class="select-container">
-            <select name="gender-select" id="gender-select">
+            <select
+              id="gender-select"
+              name="gender-select"
+              onChange= { handleGenderChange }
+            >
                 <option selected disabled>Gender</option>
-                <option value="1">Male</option>
-                <option value="2">Female</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
             </select>
         </div>
         <div class="select-container">
-            <select name="status-select" id="status-select">
+            <select
+              id="status-select"
+              name="status-select"
+              onChange= { handleStatusChange }
+            >
                 <option selected disabled>Status</option>
-                <option value="1">Alive</option>
-                <option value="2">Dead</option>
+                <option value="alive">Alive</option>
+                <option value="dead">Dead</option>
             </select>
         </div>
 
-        <div class="character-link">
-          <a href="/asdasdf">Search</a>
-        </div>
-
+        <button onClick={ handleSearch } class="primary-button">
+          Search
+        </button> 
 
     </div>
   );
